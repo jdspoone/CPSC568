a419972494d0d807f45f4b2aabf93736d3bd5ba3
Joel Nielsen <queltipo79@gmail.com>
2015-03-03 13:28:42 -0700

    Deprecated general update method

--- iRobotCreate/src/iRobotCreate/WallMeasurer.java
+++ iRobotCreate/src/iRobotCreate/WallMeasurer.java
@@ -358,7 +358,7 @@ public class WallMeasurer extends StateBasedController {
 	 * The controller examines the content of incoming inform-ref messages, and, upon notification
 	 * from one of the sensors of interest, it will trigger an event handler for that sensor update.
 	 */
-	public void update( Observable o, Object arg ) {
+	/*public void update( Observable o, Object arg ) {
         // Check that the event arg is a proper notification and not garbage
         if ( arg instanceof ObserverNotification ) {
          
@@ -402,7 +402,7 @@ public class WallMeasurer extends StateBasedController {
                 }
             }
         }
-    }
+    }*/
 	
 	//************ STATES ***************************
 	//***********************************************
